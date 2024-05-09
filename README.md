3343001984117184695089c32c796a59dc6527da
Utkarsh Kumawat <135964893+UtkarshKm@users.noreply.github.com>
2024-05-09 17:30:54 +0000

    setting up file upload , multer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learn-Backend
+
+
+## improvements
+- setting unique file names : multer.middleware
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_API_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+
+    const result = await cloudinary.uploader.upload(localFilePath,{resource_type: "auto"});
+    console.log("file uploaded" , result.url);
+    return result
+  } catch (error) {
+    fs.unlink(localFilePath) // removes the locally saved temp file as upload failed
+    return null;
+  }
+};
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
