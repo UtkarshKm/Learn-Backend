5191215ec08ccf73820f4622012e8d9ae54f5b9c
Utkarsh Kumawat <anshukumawat77955@gmail.com>
2024-05-10 22:18:02 +0530

    corrected env variable

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import fs from "fs";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_API_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -13,11 +13,13 @@ const uploadOnCloudinary = async (localFilePath) => {
       return null;
     }
 
-    const result = await cloudinary.uploader.upload(localFilePath,{resource_type: "auto"});
-    console.log("file uploaded" , result.url);
-    return result
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file uploaded", result.url);
+    return result;
   } catch (error) {
-    fs.unlink(localFilePath) // removes the locally saved temp file as upload failed
+    fs.unlink(localFilePath); // removes the locally saved temp file as upload failed
     return null;
   }
 };
