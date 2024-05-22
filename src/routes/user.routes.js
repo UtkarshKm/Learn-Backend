165277f990074a824ee9720cbb202abf8a0d19ab
Utkarsh Kumawat <135964893+UtkarshKm@users.noreply.github.com>
2024-05-22 12:02:37 +0000

    feat: Add routes and controller functions for changing user password, getting current user, updating user avatar, updating user cover image, and updating user profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import {
+  changeUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserProfile,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyJwt from "../middlewares/auth.middleware.js";
@@ -23,5 +29,11 @@ router.route("/login").post(loginUser);
 //secure route
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeUserPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-avatar").patch(verifyJwt,upload.single("update-avatar") ,updateUserAvatar)
+router.route("/update-cover").patch(verifyJwt,upload.single("updateCover"),updateUserCoverImage)
+router.route("channel/:username").get(verifyJwt , getUserChannelProfile)
+router.route("/update-user").patch(verifyJwt, updateUserProfile)
 
 export default router;
